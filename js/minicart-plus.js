527a15033b492ebed70c29912b4cfd55a41b3172
Fabrizio <fabriziotappero@users.noreply.github.com>
2015-05-29 10:07:56 +0200

    fixed some button label and reorganized thone func

--- js/minicart-plus.js
+++ js/minicart-plus.js
@@ -1,7 +1,7 @@
 /*!
  * Minicart-plus is a great PayPal shopping cart written in JavaScript.
  *
- * @version 1.1
+ * @version 1.2
  * @author Fabrizio Tappero <https://github.com/fabriziotappero>
  * @license <https://github.com/fabriziotappero/minicart-plus/blob/gh-pages/LICENSE>
  */
@@ -15,10 +15,57 @@ $(document).ready(function() {
         shipping_subtxt = "International Registered Mail",
         shipping_currency = "EUR",
         check_out_button_txt = "Check Out",
+        cont_shopping_button_txt = "Continue Shopping",
         cart_total_txt = "",
         discount_txt = "";
     /* ################################################################### */
 
+    /* restyle the shopping cart has we like */
+    function restyle_shopping_cart() {
+        /* make all quantity fields not editable */
+        $('#PPMiniCart .minicart-quantity').attr('readonly', 'readonly');
+        $('#PPMiniCart .minicart-quantity').css({
+            'background': 'white',
+            'border': '0',
+            'box-shadow': 'none'
+        });
+        /* hide shipping cost item quantity and delete button*/
+        $("#PPMiniCart .minicart-item").each(function() {
+            if ($(this).find('.minicart-name').text().toLowerCase() == shipping_txt.toLowerCase()) {
+                $(this).find('.minicart-quantity').css('visibility', 'hidden');
+                $(this).find('.minicart-remove').css('visibility', 'hidden');
+            }
+        });
+        /* style close button and check out buttons */
+        $('.minicart-submit').css({
+            'right': '148px',
+            'min-width': '90px',
+            'font-size': '15px',
+            'padding': '5px 6px',
+            'font-weight': 'normal'
+        });
+
+        $('.minicart-closer').text(cont_shopping_button_txt);
+        $('.minicart-closer').css({
+            'position': 'absolute',
+            'right': '5px',
+            'height': '33px',
+            'min-width': '90px',
+            'font-size': '15px',
+            'font-weight': 'normal',
+            'margin': '-3px -2px',
+            'padding': '5px 6px',
+            'background': 'linear-gradient(to bottom, #fff6e9 0%, #ffaa00 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
+            'border': '1px solid #ffc727',
+            'border-radius': '5px'
+        });
+        $('.minicart-closer').insertAfter('.minicart-submit');
+
+        return 0;
+    }
+
+
+    /* calcualate shipping costs and update the cart */
     function update_cart_shipping_cost(idx, product) {
         var i, ii, shop_items;
 
@@ -27,7 +74,7 @@ $(document).ready(function() {
 
         /* delete any previous shipping charge item */
         for (i = 0; i < shop_items.length; i++) {
-            if (shop_items[i].get('item_name').toLowerCase() == "shipping and handling") {
+            if (shop_items[i].get('item_name').toLowerCase() == shipping_txt.toLowerCase()) {
                 //console.log(shop_items);
                 paypal.minicart.cart.remove(i);
             }
@@ -46,7 +93,7 @@ $(document).ready(function() {
         for (i = 0; i < shop_items.length; i++) {
             cart_items_q.push(shop_items[i].get('quantity')); //shopping cart item quantity
             cart_items_n.push(shop_items[i].get('item_name')); //shopping cart item name
-          }
+        }
 
         //console.log("n", cart_items_n);
         //console.log("q", cart_items_q);
@@ -55,10 +102,10 @@ $(document).ready(function() {
         for (i = 0; i < cart_items_n.length; i++) {
             // search for the weight of cart_items_n[i] inside forms
             for (ii = 0; ii < forms.length; ii++) {
-              if(forms.children('input[name="item_name"]')[ii].value==cart_items_n[i]){
-                w = forms.children('input[name="weight_g"]')[ii].value;
-                shipping_weight_g += parseFloat(cart_items_q[i] * w);
-              }
+                if (forms.children('input[name="item_name"]')[ii].value == cart_items_n[i]) {
+                    w = forms.children('input[name="weight_g"]')[ii].value;
+                    shipping_weight_g += parseFloat(cart_items_q[i] * w);
+                }
             }
         }
         //console.log("New Shipping Weight:",shipping_weight_g);
@@ -79,43 +126,10 @@ $(document).ready(function() {
         };
         paypal.minicart.cart.add(p);
 
-        /* make all quantity fields not editable */
-        $('#PPMiniCart .minicart-quantity').attr('readonly','readonly');
-        $('#PPMiniCart .minicart-quantity').css({
-            'background': 'white',
-            'border': '0',
-            'box-shadow': 'none'
-        });
-        /* hide shipping cost item quantity and delete button*/
-        $("#PPMiniCart .minicart-item").each(function() {
-            if ($(this).find('.minicart-name').text().toLowerCase() == "shipping and handling") {
-                $(this).find('.minicart-quantity').css('visibility', 'hidden');
-                $(this).find('.minicart-remove').css('visibility', 'hidden');
-            }
-        });
-        /* style close button and check out buttons */
-        $('.minicart-submit').css({
-            'right': '155px',
-            'min-width': '90px',
-            'font-size': '13px',
-            'font-weight': 'normal'
-        });
+        /* restyle shopping cart as we like */
+        restyle_shopping_cart()
 
-        $('.minicart-closer').text('Continue Shopping');
-        $('.minicart-closer').css({
-            'position': 'absolute',
-            'right': '5px',
-            'height':'33px',
-            'min-width': '90px',
-            'font-size': '13px',
-            'font-weight': 'normal',
-            'margin': '-3px -2px',
-            'padding': '5px 7px',
-            'background': 'linear-gradient(to bottom, #fff6e9 0%, #ffaa00 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-            'border': '1px solid #ffc727',
-            'border-radius': '5px'
-        });
-        $('.minicart-closer').insertAfter('.minicart-submit');
+        return 0;
     }
 
     /* show shopping cart*/
@@ -123,43 +137,9 @@ $(document).ready(function() {
         e.stopPropagation();
         paypal.minicart.view.show();
 
-        /* make all quantity field not editable */
-        $('#PPMiniCart .minicart-quantity').attr('readonly','readonly');
-        $('#PPMiniCart .minicart-quantity').css({
-            'background': 'white',
-            'border': '0',
-            'box-shadow': 'none'
-        });
-        /* hide shipping cost item quantity and delete button*/
-        $("#PPMiniCart .minicart-item").each(function() {
-            if ($(this).find('.minicart-name').text().toLowerCase() == "shipping and handling") {
-                $(this).find('.minicart-quantity').css('visibility', 'hidden');
-                $(this).find('.minicart-remove').css('visibility', 'hidden');
-            }
-        });
-        /* style close and check out buttons */
-        $('.minicart-submit').css({
-            'right': '155px',
-            'min-width': '90px',
-            'font-size': '13px',
-            'font-weight': 'normal'
-        });
+        /* restyle shopping cart as we like */
+        restyle_shopping_cart()
 
-        $('.minicart-closer').text('Continue Shopping');
-        $('.minicart-closer').css({
-            'position': 'absolute',
-            'right': '5px',
-            'height':'33px',
-            'min-width': '90px',
-            'font-size': '13px',
-            'font-weight': 'normal',
-            'margin': '-3px -2px',
-            'padding': '5px 7px',
-            'background': 'linear-gradient(to bottom, #fff6e9 0%, #ffaa00 100%) repeat scroll 0 0 rgba(0, 0, 0, 0)',
-            'border': '1px solid #ffc727',
-            'border-radius': '5px'
-        });
-        $('.minicart-closer').insertAfter('.minicart-submit');
     });
 
     /* render shopping cart and reposition on the right*/
@@ -179,7 +159,7 @@ $(document).ready(function() {
     // every time an item is removed calculate and update the cart shipping weight
     paypal.minicart.cart.on('remove', function(idx, product) {
         // do nothing if the item removed is the shipping cost
-        if (product.get('item_name').toLowerCase() == "shipping and handling") {
+        if (product.get('item_name').toLowerCase() == shipping_txt.toLowerCase()) {
             return 0;
         }
 
@@ -189,22 +169,16 @@ $(document).ready(function() {
         return 0;
     });
 
-    // every time a item quantity is modified calculate and update the cart shipping weight
-    // not implemented because not so easy...
-    //$("#PPMiniCart .minicart-quantity").on("change keyup paste click", function(){
-    //  console.log('bingo');
-    //})
 
     // every time an item is added calculate and update the cart shipping weight
     paypal.minicart.cart.on('add', function(idx, product, isExisting) {
         // do nothing if the item added is the shipping cost
-        if (product.get('item_name').toLowerCase() == "shipping and handling") {
+        if (product.get('item_name').toLowerCase() == shipping_txt.toLowerCase()) {
             return 0;
         }
         // calculate and update shipping costs
         update_cart_shipping_cost(idx, product);
         return 0;
     });
-
 
 });
